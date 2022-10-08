92d91fa7cae1efb41624b41291b76011a5d571c4
PedroCPinto <54863021+PedroCPinto@users.noreply.github.com>
2022-10-08 02:16:10 +0100

    Add map structure and examples folder

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package astro
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/pedrocpinto/astro/pkg/constants"
+)
 
 func PrintSomething() {
 	fmt.Println("Printing something as promised")
+}
+
+func GetAstroConstant(constName string) (*constants.AstroConstant, error) {
+	return constants.Get(constName)
 }
--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -1,10 +1,37 @@
 package constants
 
-const (
-	GaussianGravitationalConstant = "gaussian_gravitational_constant"
-	LighYear = "light_year"
-	MeanRatioOfTheTTSecondToTheTCGSecond = "mean_ratio_of_the_tt_second_to_the_tcg_second"
-	MeanRatioOfTheTCBSecondToTheTDBSecond = "mean_ratio_of_the_tcb_second_to_the_tdb_second"
-	Parsec = "parsec"
-	SpeedOfLight = "speed_of_light"
+import (
+	"errors"
+	"fmt"
 )
+
+const (
+	GaussianGravitationalConstant         = "gaussian_gravitational_constant"
+	LighYear                              = "light_year"
+	MeanRatioOfTheTTSecondToTheTCGSecond  = "mean_ratio_of_the_tt_second_to_the_tcg_second"
+	MeanRatioOfTheTCBSecondToTheTDBSecond = "mean_ratio_of_the_tcb_second_to_the_tdb_second"
+	Parsec                                = "parsec"
+	SpeedOfLight                          = "speed_of_light"
+)
+
+type AstroConstant struct {
+	name  string
+	value string
+	units string
+}
+
+var astronomicalConstants = map[string]AstroConstant{
+	LighYear: {
+		name: "Light Year",
+		value: "9.46073044725808 * 10^15",
+		units: "meters",
+	},
+}
+
+func Get(constant string) (*AstroConstant, error) {
+	if val, ok := astronomicalConstants[constant]; ok {
+		return &val, nil
+	}
+
+	return nil, errors.New(fmt.Sprintf("Sorry, info for constant '%s' is not configured.", constant))
+}
