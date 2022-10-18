5b81f9e50c21eff1cbf194c04c83ea3e88642a51
PedroCPinto <54863021+PedroCPinto@users.noreply.github.com>
2022-10-18 01:04:45 +0100

    Add first unit tests

---
+++ pkg/constants/constants_test.go
@@ -0,0 +1,29 @@
+package constants
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGet(t *testing.T) {
+	testCases := []struct {
+		name         string
+		input        string
+		expectedName string
+		expectedErr  error
+	}{
+		{
+			name:         "When 'light year' is provided, the light year struct is returned",
+			input:        LighYear,
+			expectedName: "Light Year",
+			expectedErr:  nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		constant, err := Get(tc.input)
+		assert.Equal(t, tc.expectedName, constant.name)
+		assert.Nil(t, err)
+	}
+}
